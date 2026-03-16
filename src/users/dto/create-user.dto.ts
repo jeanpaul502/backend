7218a6a7c3f7b96937c037c53e4eff30083f116b
Dev <dev@fuzion.com>
2026-03-16 23:02:43 +0100

    Add validation decorators to CreateUserDto to fix 400 Bad Request error

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({}, { message: 'Email invalide' })
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6, { message: 'Le mot de passe doit faire au moins 6 caractères' })
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
 }
