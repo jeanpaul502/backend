500304feed7af648dbe0b5ab84f8b1ac061dd551
Dev <dev@fuzion.com>
2026-03-16 23:18:09 +0100

    Increase payload limit to 50mb to allow larger requests

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 
+import { json, urlencoded } from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
 
   app.use(
     helmet({
